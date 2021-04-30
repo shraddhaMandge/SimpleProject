72cb14b319344ca528e1affe4ec438ca3f4c9e3a
shraddhaMandge <82930409+shraddhaMandge@users.noreply.github.com>
2021-04-30 15:03:22 +0530

    Initial commit

---
+++ Thread/src/test/java/com/example/Thread/CachedThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachedThreadPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
